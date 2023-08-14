343478731fa14c51362dccb016273a6266c2c483
elvesbd <elvesbd@yahoo.com>
2023-08-14 12:02:20 -0300

    feat: created params validation

--- service-one/src/modules/users/dto/create-user.dto.ts
+++ service-one/src/modules/users/dto/create-user.dto.ts
@@ -1,6 +1,27 @@
-export class CreateOrderDto {
+import {
+  IsEmail,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
+  @IsString()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   phone: string;
+
+  @IsInt()
+  @IsNumber()
+  @IsNotEmpty()
   age: number;
 }
