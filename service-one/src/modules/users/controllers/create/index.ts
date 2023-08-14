2b7111859ed4b4f85c702401007189458f723e59
elvesbd <elvesbd@yahoo.com>
2023-08-14 12:07:13 -0300

    feat: implemented controller for create a user

--- service-one/src/modules/users/controllers/create/index.ts
+++ service-one/src/modules/users/controllers/create/index.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUsersService } from '../../services/create';
+import { CreateUserDto } from '../../dto/create-user.dto';
 
 @Controller('users')
-export class CreateUsersController {}
+export class CreateUsersController {
+  constructor(private readonly createUsersService: CreateUsersService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto): Promise<void> {
+    await this.createUsersService.execute(createUserDto);
+  }
+}
--- service-one/src/modules/users/services/create/index.ts
+++ service-one/src/modules/users/services/create/index.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from '../../dto/create-user.dto';
 
 @Injectable()
-export class CreateUsersService {}
+export class CreateUsersService {
+  async execute(createUsersDto: CreateUserDto): Promise<void> {
+    // Lógica para criar a ordem e publicar no Kafka
+    //await this.kafkaService.publishToTopic('order-topic', createOrderDto);
+  }
+}
