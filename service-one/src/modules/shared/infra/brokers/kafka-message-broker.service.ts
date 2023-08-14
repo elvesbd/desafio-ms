21699fc44e988760a8ccbf040f1a8c55a1ba514d
elvesbd <elvesbd@yahoo.com>
2023-08-14 13:26:14 -0300

    refactor: rename function for publish topic

--- service-one/src/modules/shared/infra/brokers/kafka-message-broker.service.ts
+++ service-one/src/modules/shared/infra/brokers/kafka-message-broker.service.ts
@@ -5,7 +5,7 @@ import { MessageBroker } from 'src/modules/users/interfaces';
 export class KafkaMessageBrokerService implements MessageBroker {
   constructor(private readonly t: any) {}
 
-  publishToTopic(topic: string, payload: object): Promise<void> {
+  publish(topic: string, payload: object): Promise<void> {
     throw new Error('Method not implemented.');
   }
 }
