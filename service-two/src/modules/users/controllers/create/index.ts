f300f9025f7b4bfe2277496603ea3ffe827fe89f
elvesbd <elvesbd@yahoo.com>
2023-08-14 18:38:44 -0300

    chore: created structure for project two

---
+++ service-two/src/modules/users/controllers/create/index.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUsersService } from '../../services/create';
+import { CreateUserDto } from '../../dto';
+
+@Controller('users')
+export class CreateUsersController {
+  constructor(private readonly createUsersService: CreateUsersService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto): Promise<void> {
+    await this.createUsersService.execute(createUserDto);
+  }
+}
