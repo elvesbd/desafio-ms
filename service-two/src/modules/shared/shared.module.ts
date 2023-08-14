d49e5a088fef0211de6b9d7f4ef25206fa000793
elvesbd <elvesbd@yahoo.com>
2023-08-14 18:23:44 -0300

    chore: install package kafkajs

--- service-two/src/modules/shared/shared.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProducerService } from './infra/brokers';
-
-@Module({
-  providers: [ProducerService],
-  exports: [ProducerService],
-})
-export class SharedModule {}
