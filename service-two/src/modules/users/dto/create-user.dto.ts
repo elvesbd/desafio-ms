65f7420df69704f69586643f259b9eab98ef2c8c
elvesbd <elvesbd@yahoo.com>
2023-08-14 21:16:54 -0300

    feat: consumer messages

--- service-two/src/modules/users/dto/create-user.dto.ts
+++ service-two/src/modules/users/dto/create-user.dto.ts
@@ -1,27 +1,6 @@
-import {
-  IsEmail,
-  IsInt,
-  IsNotEmpty,
-  IsNumber,
-  IsString,
-} from 'class-validator';
-
 export class CreateUserDto {
-  @IsString()
-  @IsNotEmpty()
   name: string;
-
-  @IsEmail()
-  @IsString()
-  @IsNotEmpty()
   email: string;
-
-  @IsString()
-  @IsNotEmpty()
   phone: string;
-
-  @IsInt()
-  @IsNumber()
-  @IsNotEmpty()
   age: number;
 }
--- service-two/src/modules/users/services/create/index.ts
+++ service-two/src/modules/users/services/create/index.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConsumerService } from 'src/modules/shared/infra/brokers/kafka/consumer.service';
 
 @Injectable()
-export class CreateUsersService {
-  constructor(private readonly producerService: ConsumerService) {}
+export class CreateUsersService implements OnModuleInit {
+  constructor(private readonly consumerService: ConsumerService) {}
 
-  async execute(): Promise<void> {}
+  async onModuleInit() {
+    await this.consumerService.consume(
+      { topics: ['create-user'] },
+      {
+        eachMessage: async ({ topic, partition, message }) => {
+          console.log({
+            value: message.value.toString(),
+            topic: topic.toString(),
+            partition: partition.toString(),
+          });
+        },
+      },
+    );
+  }
 }
