ebdcf3bb084aa43e9f9dd5a5f67d9b07689479bd
elvesbd <elvesbd@yahoo.com>
2023-08-16 11:46:47 -0300

    chore: remove imports does not used

--- ms-users/src/main.ts
+++ ms-users/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ExceptionFilter } from './modules/shared/exceptions/rpc-exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
