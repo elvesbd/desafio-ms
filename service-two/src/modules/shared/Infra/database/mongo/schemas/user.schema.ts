4c21f8f045136b421ab6e0a80e954cc32a54ce48
elvesbd <elvesbd@yahoo.com>
2023-08-16 08:35:06 -0300

    feat: created user model

---
+++ service-two/src/modules/shared/Infra/database/mongo/schemas/user.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { User } from 'src/modules/users/models/user';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class UserModel implements User {
+  @Prop()
+  id: string;
+
+  @Prop()
+  name: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  age: number;
+}
+
+export const UserSchema = SchemaFactory.createForClass(UserModel);
